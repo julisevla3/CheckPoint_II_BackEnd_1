9b02e6dceebfaf752822e1fa60be21b8a0cdbf35
Juliane Alves <41329356+julisevla3@users.noreply.github.com>
2022-09-12 23:29:24 -0300

    excluindo todas as pastas para fazer o merge na branch edimilson

--- src/main/java/com/odontologica/clinica/repository/IConsultasRepository.java
+++
@@ -1,7 +0,0 @@
-package com.odontologica.clinica.repository;
-
-import com.odontologica.clinica.entity.ConsultasEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IConsultasRepository extends JpaRepository<ConsultasEntity, Long> {
-}
--- src/main/java/com/odontologica/repository/IDentistaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.odontologica.repository;
-
-import java.util.List;
-
-public interface IDentistaRepository<T> {
-
-    public List<T> buscarTodosDentistas();
-
-}
