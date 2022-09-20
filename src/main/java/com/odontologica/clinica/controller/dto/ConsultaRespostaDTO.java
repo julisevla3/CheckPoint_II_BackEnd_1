7e8ae77efa9187c910057df9acd43cd2cdc842ca
Juliane Alves <41329356+julisevla3@users.noreply.github.com>
2022-09-19 23:32:44 -0300

    updateDTO

---
+++ src/main/java/com/odontologica/clinica/controller/dto/ConsultaRespostaDTO.java
@@ -0,0 +1,20 @@
+package com.odontologica.clinica.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ConsultaRespostaDTO {
+
+    private Date dataConsulta;
+    private LocalDateTime horaConsulta;
+    private DentistaDTO dentista;
+    private PacienteDTO paciente;
+
+}
