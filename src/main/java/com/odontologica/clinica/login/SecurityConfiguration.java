94a42c5672de5324b431e5c0b0fc29e3336eaf9f
edimilsonbraz <edimilson.gt8@gmail.com>
2022-09-22 21:12:10 -0300

    :arrow_up: Upgrade dependencies Security

--- src/main/java/com/odontologica/clinica/login/SecurityConfiguration.java
+++ src/main/java/com/odontologica/clinica/login/SecurityConfiguration.java
@@ -1,12 +1,16 @@
 package com.odontologica.clinica.login;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
     @Autowired
